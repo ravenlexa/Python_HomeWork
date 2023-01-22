7c6de05c3bd317cfbeb1443057a5b981719b3d52
Aleksey <raven.lexa@yandex.ru>
2023-01-22 16:17:53 +0300

    Task6_2

---
+++ Seminar_6/Task6_2.py
@@ -0,0 +1,18 @@
+from random import randint as RI
+
+my_list = [RI(0, 100) for i in range(8)]
+print(f'             Список: {my_list}')
+
+def shafl_list():
+    global my_list
+    for i in range(len(my_list) - 1, 0, -1):
+        j = RI(0, i + 1)
+        my_list[i], my_list[j] = my_list[j], my_list[i]
+    return my_list
+
+my_list_list = shafl_list()
+print(f'Перемешенный список: {my_list}')
+
+'''Не знаю на сколько компактней стало))
+Пытался то что в функции тоже в одну строку сделать, костыли получаются.
+Поэтому оставил так.'''
